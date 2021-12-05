5ccaaf4732ecf90d43d3075de1eb6cc0e18beca3
Rashid <rashid.aliev@sp3d.co>
2021-12-05 01:49:34 +0100

    Put pieces on a good place

--- frontend/src/components/Board/Board.js
+++ frontend/src/components/Board/Board.js
@@ -4,8 +4,31 @@ import './Board.css';
 import Tile from '../Tile/Tile'
 
 const Board = () => {
-
-  return <div id="board"><Tile ></Tile></div>
+  let board = [];
+  let redPiece=0;
+  let greenPiece=1;
+  for (let i=0; i < 5; i++)
+  {
+    board.push(
+      <div id={`redPiece${i}`}>
+        <Tile piece={redPiece} />
+      </div>
+    )
+  }
+  for (let i=0; i < 5; i++)
+  {
+    board.push(
+      <div id={`greenPiece${i}`}>
+        <Tile piece={greenPiece} />
+      </div>
+    )
+  }
+  
+  return (
+    <div id="board">
+      {board}
+    </div>
+  );
 }
 
 export default Board;
--- frontend/src/components/Tile/Tile.js
+++ frontend/src/components/Tile/Tile.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import './Tile.css';
+import greenPiece from "../../assets/images/green_piece.png";
+import redPiece from "../../assets/images/red_piece.png";
 
-const Tile = () => {
-  return <div id="tile"><img src="assets/images/green_piece.png"/></div>
+
+const Tile = ({piece}) => {
+  if(piece==0) return <img src={redPiece} alt="red piece"/>
+  else return <img src={greenPiece} alt="green piece"/>
 }
 
 export default Tile;
