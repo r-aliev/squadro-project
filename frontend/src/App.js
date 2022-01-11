098f1a6f4c649508e00514467253532386fad608
Rashid <rashid.aliev@sp3d.co>
2022-01-11 19:49:29 +0100

    Correct structure and beautify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import {Route, Routes} from 'react-router-dom'
-
-// Pages
-
-import Home from "./pages/Home" 
-import Game from "./pages/Game" 
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./components/Home/Home";
+import Game from "./components/Game/Game";
 
 import "./App.css";
 
-function App()  {
-	return (
-		<Router>
-		<Routes>
-
-			
-		<Route exact path="/"  element={<Home/>}/>
-    <Route exact path="/game"  element={<Game/>}/>
-		
-		
-		</Routes>
+function App() {
+  return (
+    <Router>
+			<div className="App">
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/game" element={<Game />} />
+				</Routes>
+			</div>
 		</Router>
-	)
+
+  );
 }
 
 export default App;
