d52903e8b29f7f40013e9e0b43c99a128d272732
tinou974 <etiennepaita@gmail.com>
2022-01-12 14:42:41 +0100

    home page layout and parameters selection

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "../Header/Header";
 import Game from "../Game/Game";
 import "./Home.css";
+import HomeForm from "./HomeForm";
 import { Routes, Route } from "react-router-dom";
+
+const Login = details => {
+  console.log(details);
+}
 
 const Home = () => {
   return (
     <div className="Home">
       <Header />
+      <p id="gameExplanation">Squadro est un jeu de déplacement et de stratégie en un contre un. Chaque joueur possède 5 pièces et doit ramener 4 de ses pièces à son emplacement d’origine pour remporter la partie.</p>
+      <HomeForm Login={Login}/>
+      
       <Routes>
         <Route path="/game" element={Game}></Route>
       </Routes>
