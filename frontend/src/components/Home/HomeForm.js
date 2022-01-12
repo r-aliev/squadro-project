6b6acc2acc27084b4b27629af562c4be3a8e247e
tinou974 <etiennepaita@gmail.com>
2022-01-12 21:32:14 +0100

    updates

--- frontend/src/components/Home/HomeForm.js
+++ frontend/src/components/Home/HomeForm.js
@@ -18,6 +18,7 @@ function HomeForm({Login}) {
         Login(details);
         //TODO : if(pseudo != "")  Open Game page 
         //       else Error message
+        if(details.pseudo != "") window.open("../Game/Game");
     }
 
     const onBoardClick = e => {
@@ -46,6 +47,23 @@ function HomeForm({Login}) {
         }
     }
 
+    const onLevelClick = e => {
+        if(e.target.id === "easyBtn"){
+            document.getElementById("easyBtn").style.backgroundColor = "#ffffff"
+            document.getElementById("easyBtn").style.color = "#FB5BCE"
+            document.getElementById("hardBtn").style.backgroundColor = "#b9b9b9"
+            document.getElementById("hardBtn").style.color = "#6d6d6d"
+            setDetails({...details, level: "Easy"})
+        }
+        else{
+            document.getElementById("hardBtn").style.backgroundColor = "#ffffff"
+            document.getElementById("hardBtn").style.color = "#FB5BCE"
+            document.getElementById("easyBtn").style.backgroundColor = "#b9b9b9"
+            document.getElementById("easyBtn").style.color = "#6d6d6d"
+            setDetails({...details, level: "Hard"})
+        }
+    }
+
     return (
         <form className="customForm" onSubmit={submitHandler}>
             <div className="mainContainer">
@@ -56,13 +74,13 @@ function HomeForm({Login}) {
                 </div>
 
                 <div className="midPack">
-                    <div className="pinkLabel">
+                    <div className="pinkLabel" id="pseudoArea">
                         <label >Pseudo :</label>
                         <input type="text" name="pseudoInput" id="pseudoInput" onChange={e => setDetails({...details, pseudo: e.target.value})} value={details.pseudo}/>
                     </div>
                     <div className="difficulty">
-                        <button type="button" className="pinkLabel" id="easyBtn" onClick={e => setDetails({...details, level: "Easy"})}>Easy</button>
-                        <button type="button" className="pinkLabel" id="hardBtn" onClick={e => setDetails({...details, level: "Hard"})}>Hard</button>
+                        <button type="button" className="buttonStandard" id="easyBtn" onClick={onLevelClick}>Easy</button>
+                        <button type="button" className="buttonStandard" id="hardBtn" onClick={onLevelClick}>Hard</button>
                     </div>
                 </div>
 
@@ -75,7 +93,7 @@ function HomeForm({Login}) {
                 </div>
             </div>
 
-            <input className="pinkLabel" type="submit"  name="playBtn" id="playBtn" value="JOUER"/>
+            <input className="buttonStandard" type="submit"  name="playBtn" id="playBtn" value="JOUER"/>
         </form>
     );
 
