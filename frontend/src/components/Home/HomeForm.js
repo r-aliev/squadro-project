e850b78bf37b5aebbfd6dc7ff2bbd694c476e837
tinou974 <etiennepaita@gmail.com>
2022-01-13 12:01:36 +0100

    UPDATE-HOME : jeu en local ou vs AI

--- frontend/src/components/Home/HomeForm.js
+++ frontend/src/components/Home/HomeForm.js
@@ -11,7 +11,7 @@ import red_piece_selected from "../../assets/images/red_piece_selected.png";
 import red_piece_unselected from "../../assets/images/red_piece_unselected.png";
 
 function HomeForm({Login}) {
-    const [details, setDetails] = useState({pseudo: "", boardColor: "Black", level: "Easy", side: "Red"});
+    const [details, setDetails] = useState({pseudo: "", boardColor: "Black", mode: "Local", level: "Easy", side: "Red"});
 
     const submitHandler = e => {
         e.preventDefault();
@@ -64,6 +64,34 @@ function HomeForm({Login}) {
         }
     }
 
+    const onModeClick = e => {
+        if(e.target.id === "localBtn"){
+            document.getElementById("localBtn").style.backgroundColor = "#ffffff"
+            document.getElementById("localBtn").style.color = "#FB5BCE"
+            document.getElementById("aiBtn").style.backgroundColor = "#b9b9b9"
+            document.getElementById("aiBtn").style.color = "#6d6d6d"
+            setDetails({...details, mode: "Local"})
+        }
+        else{
+            document.getElementById("aiBtn").style.backgroundColor = "#ffffff"
+            document.getElementById("aiBtn").style.color = "#FB5BCE"
+            document.getElementById("localBtn").style.backgroundColor = "#b9b9b9"
+            document.getElementById("localBtn").style.color = "#6d6d6d"
+            setDetails({...details, mode: "AI"})
+        }
+
+        var state = document.getElementById("difficulty").style.display;
+        if (state == 'flex' && e.target.id === "localBtn") {
+            document.getElementById("difficulty").style.display = 'none';
+            document.getElementById("midPack").style.marginBottom = "50px";
+        } else if (state != 'flex' && e.target.id === "aiBtn") {
+            document.getElementById("difficulty").style.display = 'flex';
+            document.getElementById("midPack").style.marginBottom = "0px";
+        }
+
+        
+    }
+
     return (
         <form className="customForm" onSubmit={submitHandler}>
             <div className="mainContainer">
@@ -73,12 +101,16 @@ function HomeForm({Login}) {
                     <img id="whiteBoard" src={white_board_unselected} alt="" onClick={onBoardClick}></img>
                 </div>
 
-                <div className="midPack">
+                <div className="midPack" id="midPack">
                     <div className="pinkLabel" id="pseudoArea">
                         <label >Pseudo :</label>
                         <input type="text" name="pseudoInput" id="pseudoInput" onChange={e => setDetails({...details, pseudo: e.target.value})} value={details.pseudo}/>
                     </div>
-                    <div className="difficulty">
+                    <div className="gameMode">
+                        <button type="button" className="buttonStandard" id="localBtn" onClick={onModeClick}>Jeu local</button>
+                        <button type="button" className="buttonStandard" id="aiBtn" onClick={onModeClick}>vs AI</button>
+                    </div>
+                    <div className="difficulty" id="difficulty">
                         <button type="button" className="buttonStandard" id="easyBtn" onClick={onLevelClick}>Easy</button>
                         <button type="button" className="buttonStandard" id="hardBtn" onClick={onLevelClick}>Hard</button>
                     </div>
